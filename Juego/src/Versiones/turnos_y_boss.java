af2b0598477c4ec9548caac701a2dbf4ceb5eb16
GastonTH <44206933+GastonTH@users.noreply.github.com>
2018-11-29 13:51:46 +0100

    trunos_y_objetosMatriz
    objetos up
    turnos opciones up
    defensa up

--- Juego/src/Versiones/turnos_y_boss.java
+++ Juego/src/Versiones/turnos_y_boss.java
@@ -34,10 +34,47 @@ public class turnos_y_boss {
 		}
 		System.out.printf("%n");
 
-	
 	}
 	
+	public static void menuRPG () {
+		System.out.println("||--------------Que quieres hacer----------------||");
+		System.out.println("|| - Atacar - - Defender - - Magia - - Objetos - ||");		
+		System.out.println("||----- --------/////////////////----------------||");
+	}
 	
+	public static void menuObjetos () {
+		
+		System.out.println("||---------------Que quieres usar----------------||");
+
+		System.out.println("||----- --------/////////////////----------------||");
+
+		
+	}
+	
+	public static void items () {
+		
+		String [][] listaItems = new String [4][2];
+		
+		listaItems[0][0] ="1";
+		listaItems[0][1] ="Elixir de vida";
+		listaItems[1][0] ="2";
+		listaItems[1][1] ="Elixir de mana";
+		listaItems[2][0] ="3";
+		listaItems[2][1] ="Elixir de fuerza";
+		listaItems[3][0] ="4";
+		listaItems[3][1] ="Elixir de defensa";
+		
+		for (int i = 0; i < listaItems.length; i++) {
+			
+			for (int j = 0; j < listaItems[0].length; j++) {
+				
+				System.out.printf("%4s   ",listaItems[i][j]);
+				
+			}
+			System.out.printf("%n");
+		}
+		
+	}
 
 	public static void main(String[] args) {
 
@@ -53,15 +90,25 @@ public class turnos_y_boss {
 		String boss = "Bic";
 		String eleccion;
 		
+		/*Variables del personaje*/
+		
 		int ataqueOrigPers=20;
 		int ataquePers=ataqueOrigPers;
+		int defensaPJ = 10;/*al ataque se le rensta la defensa*/
 		//
 		int vitPJ = 300;
+		int manaPJ;
+		/*                       */
+		
+		/*Variables del boss*/
 		
 		int ataqueOrigBoss = 10;
 		int ataqueBoss=ataqueOrigBoss;
+		int defensaBoss = 30;/*al ataque se le rensta la defensa*/
 		//
 		int vitBoss = 300;
+		int manaBoss;
+		/*                        */
 
 		/*int numero = (int) (Math.random() * 2) + 0;*/
 		int numero;
@@ -71,6 +118,49 @@ public class turnos_y_boss {
 		
 		
 		
+		
+		
+		
+		menuRPG();
+		System.out.println("");
+		
+		menuObjetos();
+		System.out.println("");
+		
+		items();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		
 		do {
 			
 			do {
@@ -78,9 +168,8 @@ public class turnos_y_boss {
 				
 				vidaMostrar(caracter,pj,boss,vitBoss,vitPJ);
 				
-				System.out.println("Que quieres hacer:"
-						+ " - Ataque."
-						+ " - Defender.");
+				menuRPG();
+				
 				eleccion=sc.nextLine();
 				eleccion=eleccion.toLowerCase();
 
@@ -120,18 +209,18 @@ public class turnos_y_boss {
 			numero=rnd.nextInt(2);
 			System.out.println(numero);
 			
-			if (numero==0/*ataque*/) {
+			if (numero==0) {
 				
 				vitPJ-=ataqueBoss;
 				System.out.println("El boss a atacado.");
 				
 			}else {
 				
-				if (numero==234535/*defensa*/) {
+				if (numero==234535) {
 					
 					System.out.println("El boss se defiende");
 					
-				}else/*se ha dormido*/ {
+				}else {
 					
 					System.out.println("Se ha dormido");
 					
@@ -161,7 +250,7 @@ public class turnos_y_boss {
 		}
 
 
-
+*/
 
 
 	}
